197ccff2390082f16358b4033a8705df45898393
Vyacheslav Kartashov <kartashov.developer@gmail.com>
2023-06-10 00:41:09 +0300

    added color events to footer, refactored code

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-// return div and button
+// return text, button, and input
 const btnColor = document.querySelector("#btn-color");
 const txtColor = document.querySelector("#txt-color");
 const headingColor = document.querySelector("h1");
+const inputColor = document.querySelector("input");
+
+// Changing input color dynamic
+// function for creating hex range for every color
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+    }
+// function for converting rgb to hex
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+
+}
 
 // Randoming color event on click
 btnColor.addEventListener("click", randomColor);
@@ -19,19 +32,37 @@ function randomColor() {
     // Merge into final color
     const finalColor = `rgb(${redRange}, ${greenRange}, ${blueRange})`;
 
-    document.body.style.background = finalColor;
+    // Adding finalColor for changing
+    document.body.style.backgroundColor = finalColor;
     headingColor.style.color = finalColor;
-    btnColor
-    .style.color = finalColor;
+    btnColor.style.color = finalColor;
+
     // Changing text on every click
     txtColor.textContent = finalColor;
     
-
-    // For better view changing text color
-    if (redRange < 100 && greenRange < 100 && blueRange < 100) {
-        txtColor.style.color = "#fff";
-
-    } else {
-        txtColor.style.color = "#000";
-    };
+    inputColor.value = rgbToHex(redRange, greenRange, blueRange);
 };
+
+// Changing background on changing input
+inputColor.oninput = (event) => {
+    document.body.style.backgroundColor = inputColor.value; //bg changing
+    headingColor.style.color = inputColor.value; //heading changing
+    btnColor.style.color = inputColor.value; //btn changing
+    // Creating rgb format
+    const color = event.target.value ;
+    const r = parseInt(color.substr(1,2), 16)
+    const g = parseInt(color.substr(3,2), 16)
+    const b = parseInt(color.substr(5,2), 16)
+    // Changing text
+    txtColor.textContent = `rgb(${r}, ${g}, ${b})`;
+}
+
+// Changing background by items
+function color(z) {
+    let a = getComputedStyle(z);
+    let b = a.backgroundColor;
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = b;
+    headingColor.style.color = b;
+    btnColor.style.color = b;
+    txtColor.textContent = b;
+}
